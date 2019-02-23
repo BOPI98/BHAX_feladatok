3a4b64407b2b79eb63c977a66c24ce253c4614f0
BOPI98 <43614628+BOPI98@users.noreply.github.com>
2019-02-23 15:24:30 +0100

    Update 1vegtelen.c

--- 1vegtelen.c
+++ 1vegtelen.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-int main()
+#include <stdio.h>      //alapvető input és output függvényeket biztosítja
+int main()              //a program fő függvénye
 {
-int a=0;
-while(a==0)
+int a=0;                //egy egész szám deklarálása
+while(a==0)             //elöl tesztelő ciklus
 {
 
 }
-return 0;
+return 0;               //a program végi helyes lefutást jelző visszatérő érték
 }
